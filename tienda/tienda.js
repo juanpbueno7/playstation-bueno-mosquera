907082e10761e371c5f9f0fac69ccad8bcc9db3a
Your Name <you@example.com>
2023-04-26 17:50:10 -0500

    Added minor bugs

--- tienda/tienda.js
+++ tienda/tienda.js
@@ -1,41 +1,35 @@
 let items, itemsFilter, itemSelect, filterInput;
 
-let obj = {
-        "name": "PlayStation 5",
-        "desc": "Welcome to the gaming experience of the future! With its powerful AMD Zen 2 processor and AMD Radeon RDNA 2 graphics card, the PS5 offers a faster and smoother gaming experience. And not only that, but the console's futuristic design immerses you even more in the game. The PS5 is the culmination of years of innovation and evolution in the industry. Join me on the adventure and see what the PlayStation 5 can do!",
-        "price": 499.99,
-        "date": "12/11/2020",
-        "cat": "Consoles",
-        "col": ["White", "Black"],
-        "img": ["assets/ps5.webp"]
-}
 
-let nameHTML = document.getElementById("tienda-seccion-nombre");
-nameHTML.textContent = obj.name;
+
 
 filterInput = document.getElementById("store-filter");
 filterInput.addEventListener('click', catFilter, {once:false});
 let fValue = filterInput.value;
 
-function returnDetails() {
-    console.log(obj)
-    setDetailsInfo(obj)
+function returnDetails(obj__) {
+    console.log(obj__.name);
+    setDetailsInfo((obj__));
 }
-
 
 function setDetailsInfo(obj__) {
-    let specifications = document.getElementById("specific-product");
-    specifications.innerHTML= `  <img id= "img-producto" src="../${obj__.img[0]}" alt="">        
-    <div id="specific-info">
-        <h1 id="specific-name"> ${obj__.name}</h1>
-        <h2>${obj__.price}</h2>
-        <p>${obj__.cat}</p>
-        <p>${obj__.desc}</p>
-    </div>";`
+    let obj =  obj__;
+    console.log(obj.name)
+    if(obj__) {
+        let specifications = document.getElementById("specific-product");
+        specifications.innerHTML= `  <img id= "img-producto" src="../${obj__.img[0]}" alt="">        
+        <div id="specific-info">   
+            <h1 id="specific-name"> ${obj__.name}</h1>
+            
+       
+        </div>";`
+      
+        let closeWindow = document.getElementById("product-section").style.display = "none";
+      
+    }
+    
+  }
 
-    let closeWindow = document.getElementById("product-section").style.display = "none";
-
-}
 
 function catFilter() {
 	fValue = filterInput.value;
@@ -47,10 +41,12 @@ function updateItems() {
 	console.log(items);
 	console.log(itemsFilter);
 
+    //Borra todos los ojetos para que se pueda actualizar
 	while (itemList.firstChild){
 		itemList.removeChild(itemList.firstChild);
 	}
 
+    //Actualizar los objetos basados en el filtro
 	for (let j = 0; j < itemsFilter.length; j++){
 		if(itemsFilter[j].cat === fValue || fValue === "") {
 			let article = document.createElement("article");
@@ -75,9 +71,16 @@ function updateItems() {
 
 			section.append(p);
 
+            console.log()
+
+        
+
 			let button = document.createElement("button");
-			button.setAttribute("onclick","returnDetails()");
-			button.textContent = "Learn More";
+
+            let myObJ = itemsFilter[j];
+            console.log(myObJ)
+			button.setAttribute("onclick",`() => {returnDetails(${myObJ})} `);
+			button.textContent = "Buy";
 
 			section.append(button);
 
